389507024ea81ea7423bedc68a46d78a17762cd9
Sohel Rana <mdsrana11@gmail.com>
2023-11-27 16:46:34 +0600

    developed some 'views' files using HBS npm

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
- console.log(__dirname)
+ console.log(__dirname)   //to see the current path
  console.log(__filename)
 
 const app = express()
@@ -33,9 +33,39 @@ const app = express()
 
 // Creating some static web pages
 
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, '../public')   //to access all the files from public folder
+
+app.set('view engine', 'hbs')  // using for rendering the file
 
 app.use(express.static(publicDirectoryPath))
+
+app.get('/index', (req, res) => {   //rendering the index file
+      res.render('index', {
+        title: 'Weather App',
+        name: 'Sohel'
+      })
+})
+
+app.get('/about', (req, res) => {   //rendering the about file
+     res.render('about', {
+         title: 'About myself',
+         name: 'Mr. Sohel'
+     })
+})
+
+app.get('/help', (req, res) => {   //rendering the help file
+     res.render('help', {
+        title: 'Help page',
+        name: 'Mr. Sohel'
+     })
+})
+
+// app.get('/weather', (req, res) => {
+//     res.send({
+//         forecast: 'It is snowing',
+//         location: 'Dhaka'
+//     })
+// })
 
 app.listen(3000, () => {
     console.log('Server on port 3000.')
