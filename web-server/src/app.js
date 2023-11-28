7603103d1a8862a5ce0ecb9390004b4fcf861062
Sohel Rana <mdsrana11@gmail.com>
2023-11-28 14:51:01 +0600

    write some CSS code to design all pages  and add icon to the pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,58 +1,70 @@
+// Run all the files together we have to use this command ------nodemon src/app.js -e js,hbs
+
+
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 
- console.log(__dirname)   //to see the current path
- console.log(__filename)
-
 const app = express()
 
-// Define path for Express Engine
-const publicDirectoryPath = path.join(__dirname, '../public')   
-const viewsPath = path.join(__dirname, '../templates/views') //customized templates
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-//Setup handelbars engine and views location
-app.set('view engine', 'hbs')  // using for rendering the file
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-//Setup static directory to serve
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('/index', (req, res) => {   //rendering the index file
-      res.render('index', {
-        title: 'Weather App',
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
         name: 'Sohel'
-      })
+    })
 })
 
-app.get('/about', (req, res) => {   //rendering the about file
-     res.render('about', {
-         title: 'About myself....',
-         name: 'Mr. Sohel'
-     })
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sohel'
+    })
 })
 
-app.get('/help/*', (req, res) => { //Wildcat method to access any file afer the power slush
-       res.render('404', {
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Sohel'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
         title: '404',
         name: 'Sohel',
-        errorMessage: 'Help article not found'
-       })
+        errorMessage: 'Help article not found.'
+    })
 })
+
 app.get('*', (req, res) => {
-   res.render('404', {
-      title: '404',
-      name: 'Sohel',
-      errorMessage: 'Page not found.'
-   })
+    res.render('404', {
+        title: '404',
+        name: 'Sohel',
+        errorMessage: 'Page not found.'
+    })
 })
 
 app.listen(3000, () => {
-    console.log('Server on port 3000.')
+    console.log('Server is up on port 3000.')
 })
-
-
-
-// Run all the files together we have to use this command ------nodemon src/app.js -e js,hbs
